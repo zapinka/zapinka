30f1791330c713e3f20429d13f207dad70458628
Alexandr Teder <glavtortikgog@gmail.com>
2022-04-10 01:38:22 +0300

    sql

---
+++ SQL/2 Teder.sql
@@ -0,0 +1,450 @@
+create table employees(
+id serial primary key,
+eployees_name varchar (30) not null
+);
+
+select * from employees
+
+
+insert into employees (id, eployees_name, email)
+--values(default, 'Alex Figov');
+--values(default, 'Aska Tan', 'asa@gmail.com');
+--values(default, 'Anna Leshko', 'ALa@gmail.com');
+--values(default, 'Kinder Name', 'asdas@fsd.com');
+--values(default, 'Zhevachka Ollo', 'zollo@gmail.com');
+--values(default, 'Zayka', 'zayka@gmail.com');
+--values(default, 'Ezhik Lesnoy', 'lesdom@gmail.com');
+--values(default, 'Lgt', 'vrem9testov@gmail.com');
+--values(default, 'Lesnik Izgoy', 'ssss@gmail.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Christopher Mitchell','tlinden@aol.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Ronald Wilson','aardo@verizon.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Anne Martin','munge@live.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Glen Garcia','mnemonic@optonline.net');
+values(default, 'Elizabeth Johnson','osrin@att.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Jackie Coleman','bockelboy@msn.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Anna Tucker','treit@optonline.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Elizabeth Dixon','mglee@yahoo.ca');
+insert into employees (id, eployees_name, email)
+values(default, 'Rose Hernandez','barjam@live.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Joseph Diaz','teverett@optonline.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Allison Williams','hachi@verizon.net');
+insert into employees (id, eployees_name, email)
+values(default, 'James White','jshirley@optonline.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Juanita Scott','slanglois@live.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Ralph Wilkins','lipeng@me.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Darryl Richardson','bolow@mac.com');
+insert into employees (id, eployees_name, email)
+values(default, 'George Swanson','rfisher@icloud.com');
+insert into employees (id, eployees_name, email)
+values(default, 'William Kim','carroll@outlook.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Andre Holt','melnik@verizon.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Maria Sims','mschwartz@yahoo.ca');
+insert into employees (id, eployees_name, email)
+values(default, 'Ricardo Bailey','mccurley@aol.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Jennifer Perry','mmccool@optonline.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Donald Roberts','tattooman@gmail.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Mary Bowers','dprice@mac.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Maria Elliott','techie@yahoo.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Roberto Flores','intlprog@icloud.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Angela Diaz','muzzy@gmail.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Tammy Woods','eidac@yahoo.ca');
+insert into employees (id, eployees_name, email)
+values(default, 'Ray Bennett','scarlet@yahoo.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Jeffrey Andrews','weazelman@att.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Patsy Soto','kostas@optonline.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Rhonda Patterson','wikinerd@me.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Patsy Hayes','daveewart@hotmail.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Linda Martinez','carreras@att.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Tommy Paul','mastinfo@verizon.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Barbara Taylor','bcevc@yahoo.ca');
+insert into employees (id, eployees_name, email)
+values(default, 'Barbara Klein','drewf@optonline.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Valerie Griffin','grolschie@verizon.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Tiffany May','josephw@msn.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Patricia Gray','mrobshaw@msn.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Becky Howard','microfab@aol.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Bradley Walsh','tubajon@msn.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Robert Smith','pappp@hotmail.com');
+insert into employees (id, eployees_name, email)
+values(default, 'William Tate','malvar@verizon.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Penny Ross','kludge@icloud.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Joseph Hall','bhima@att.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Clarence Barrett','martyloo@yahoo.ca');
+insert into employees (id, eployees_name, email)
+values(default, 'Glenn Wilkins','dwsauder@msn.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Paul White','errxn@hotmail.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Eugene Hughes','padme@yahoo.ca');
+insert into employees (id, eployees_name, email)
+values(default, 'Nina Baker','dartlife@mac.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Linda Smith','squirrel@mac.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Amanda Jones','earmstro@yahoo.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Heather Simpson','scotfl@aol.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Oscar Guzman','ozawa@outlook.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Tonya Reed','mastinfo@aol.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Julie McKinney','feamster@aol.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Helen Williams','manuals@optonline.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Edna Sullivan','calin@yahoo.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Allen Morton','roamer@verizon.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Heidi Kelly','malin@mac.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Laura Ward','empathy@sbcglobal.net');
+insert into employees (id, eployees_name, email)
+values(default, 'David Smith','conteb@sbcglobal.net');
+insert into employees (id, eployees_name, email)
+values(default, 'Louise Jones','frode@msn.com');
+insert into employees (id, eployees_name, email)
+values(default, 'James Henry','jschauma@live.com');
+insert into employees (id, eployees_name, email)
+values(default, 'Dennis Gregory','rnelson@live.com');
+
+
+create table salary(
+id serial primary key,
+monthly_salary Int not null
+);
+
+select * from salary; 
+
+insert into salary (id, monthly_salary)
+values(default, 1000);
+insert into salary (id, monthly_salary)
+values(default, 1100);
+insert into salary (id, monthly_salary)
+values(default, 1200);
+insert into salary (id, monthly_salary)
+values(default, 1300);
+insert into salary (id, monthly_salary)
+values(default, 1400);
+insert into salary (id, monthly_salary)
+values(default, 1500);
+insert into salary (id, monthly_salary)
+values(default, 1600);
+insert into sa1ary (id, monthly_salary)
+values(default, 1600);
+insert into salary (id, monthly_salary)
+values(default, 1700);
+insert into salary (id, monthly_salary)
+values(default, 1800);
+insert into salary (id, monthly_salary)
+values(default, 1900);
+insert into salary (id, monthly_salary)
+values(default, 2100);
+insert into salary (id, monthly_salary)
+values(default, 2200);
+insert into salary (id, monthly_salary)
+values(default, 2300);
+insert into salary (id, monthly_salary)
+values(default, 2400);
+insert into salary (id, monthly_salary)
+values(default, 2500);
+insert into salary (id, monthly_salary)
+values(default, 2600);
+insert into salary (id, monthly_salary)
+values(default, 2700);
+
+
+
+select * from employee_salary;
+
+create table employee_salary(
+id serial primary key,
+employee_id int not null unique,
+salary_id int not null
+);
+
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 3,1);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 1,2);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 5,3);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 40,4);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 23,5);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 31,6);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 11,7);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 52,9);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 15,8);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 26,10);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 13,11);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 2,12);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 4,13);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 6,14);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 7,15);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 8,16);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 114,17);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 113,18);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 112,19);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 111,20);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 15,2);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 16,5);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 17,7);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 18,6);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 19,8);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 20,9);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 21,11);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 70,5);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 69,7);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 56,9);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 51,10);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 50,11);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 124,15);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 123,18);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 122,19);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 120,20);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 119,18);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 117,10);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 116,8);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 115,9);
+insert into employee_salary(id, employee_id,salary_id)
+values(default, 88,5);
+
+
+select * from roles;
+select COUNT(*) from roles;
+
+
+create table roles(
+id serial primary key,
+role_name int varchar(30) not null unique
+);
+
+insert into roles(id, role_name)
+values(default, 'Junior python developer');
+insert into roles(id, role_name)
+values(default, 'Middle Python developer');
+insert into roles(id, role_name)
+values(default, 'Senior Python developer');
+insert into roles(id, role_name)
+values(default, 'Junior Java developer');
+insert into roles(id, role_name)
+values(default, 'Middle Java developer' );
+insert into roles(id, role_name)
+values(default, 'Senior Java developer' );
+insert into roles(id, role_name)
+values(default, 'Junior JavaScript developer' );
+insert into roles(id, role_name)
+values(default, 'Middle JavaScript developer');
+insert into roles(id, role_name)
+values(default, 'Senior JavaScript developer' );
+insert into roles(id, role_name)
+values(default, 'Junior Manual QA engineer ');
+insert into roles(id, role_name)
+values(default, 'Middle Manual QA engineer ');
+insert into roles(id, role_name)
+values(default, 'Senior Manual QA engineer' );
+insert into roles(id, role_name)
+values(default, 'Project Manager' );
+insert into roles(id, role_name)
+values(default, 'Designer' );
+insert into roles(id, role_name)
+values(default, 'HR');
+insert into roles(id, role_name)
+values(default, 'CEO' );
+insert into roles(id, role_name)
+values(default, 'Sales manager' );
+insert into roles(id, role_name)
+values(default, 'Junior Automation QA engineer' );
+insert into roles(id, role_name)
+values(default, 'Middle Automation QA engineer' );
+insert into roles(id, role_name)
+values(default, 'Senior Automation QA engineer' );
+
+create table roles_employee(
+id serial primary key,
+employee_id int not null unique,FOREIGN KEY (employee_id) references employees (id),
+role_id int not null, FOREIGN KEY (role_id) references roles (id)
+);
+
+
+select * from roles_employee;
+
+update roles_employee set role_id = 5 where id = 5;
+
+insert into roles_employee(id, employee_id, role_id)
+values(default, 2,2 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 7,2 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 20,4 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 3,9);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 5,13 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 23,4 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 11,2 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 10,9 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 22,13 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 21,3 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 34,4 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 6,4 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 5,5 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 6,7 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 7,8 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 9,10 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 11,11 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 12,12 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 13,13 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 44,9 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 89,6 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 52,8 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 77,1 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 99,8 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 54,6 );
+insert into roles_employee(id, employee_id, role_id)
+values(default, 88, 5);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 60, 5);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 63, 5);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 84, 16);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 48, 10);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 102, 5);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 78, 18);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 62, 5);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 77, 19);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 82, 11);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 72, 14);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 46, 13);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 50, 16);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 58, 7);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 68, 7);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 81, 8);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 73,8);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 27, 5);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 17, 10);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 49, 12);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 52, 19);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 88, 20);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 71, 9);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 41, 22);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 34, 21);
+insert into roles_employee(id, employee_id, role_id)
+values(default, 33, 5);
